6a5075d3f1b0f50d4ffdad96130b4c4386f70e6d
Samudraneel <samudraneel.das@students.iiit.ac.in>
2020-11-14 10:56:31 +0530

    first branch commit test

--- python_blockchain_app-ibm_blockchain_post-2/app/__init__.py
+++ python_blockchain_app-ibm_blockchain_post-2/app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-from app import views
+# from app import views
+
+def create_app():
+    app = Flask(__name__)
+    from app import views
