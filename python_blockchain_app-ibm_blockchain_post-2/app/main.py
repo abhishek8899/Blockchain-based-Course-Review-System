138c825259d36685e3f19c0020169804946b2d82
Sajal Asati <sajalasati@google.com>
2020-11-18 16:58:15 +0530

    ui minor change

--- python_blockchain_app-ibm_blockchain_post-2/app/main.py
+++ python_blockchain_app-ibm_blockchain_post-2/app/main.py
@@ -181,6 +181,7 @@ def course_search():
     posts = [tag_with_sentiment(post) for post in posts]
     print(posts[:2])
     summ_text = ""
+    summ_sentiment = ""
 
     if current_course != '*':
         posts = [
@@ -194,6 +195,7 @@ def course_search():
         except:
           # print("lol",current_course,text)
           pass
+        summ_sentiment = get_sentiment(summ_text)
 
     form = SearchForm(request.form)
     return render_template('course_search.html',
@@ -205,6 +207,7 @@ def course_search():
                            node_address=CONNECTED_NODE_ADDRESS,
                            readable_time=timestamp_to_string,
                            all_courses='*',
+                           sentiment=summ_sentiment,
                            form=form)
 
 @main.route('/search_submit', methods=['POST'])
@@ -225,6 +228,36 @@ def sumz(text):
     summ_words = summarize(text, word_count = 80) 
     print("Word count summary") 
     return summ_words 
+
+def get_sentiment(text): 
+
+  # Create a SentimentIntensityAnalyzer object. 
+  sid_obj = SentimentIntensityAnalyzer() 
+
+  # polarity_scores method of SentimentIntensityAnalyzer 
+  # oject gives a sentiment dictionary. 
+  # which contains pos, neg, neu, and compound scores. 
+  sentiment_dict = sid_obj.polarity_scores(text)
+  
+  # print("Overall sentiment dictionary is : ", sentiment_dict) 
+  # print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative") 
+  # print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral") 
+  # print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive") 
+
+  # print("Sentence Overall Rated As", end = " ") 
+  
+  senti = ""
+  # decide sentiment as positive, negative and neutral 
+  if sentiment_dict['compound'] >= 0.05 : 
+    senti='positive'
+
+  elif sentiment_dict['compound'] <= - 0.05 : 
+    senti='negative'
+
+  else : 
+    senti='neutral' 
+  
+  return senti
 
 def tag_with_sentiment(post): 
 
